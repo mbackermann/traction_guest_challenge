e5bfbe4e5bee16ac15075210a667818ac25cf339
Maurício Ackermann <mabiancoac@gmail.com>
2019-03-24 12:58:24 -0300

    room model created and tested

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  it "is valid with valid attributes"
-  it "is not valid without a name"
+  subject {
+    described_class.new(name: 'São Paulo')
+  }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
